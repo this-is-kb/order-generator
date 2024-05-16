08c408806720404e381888acc69be9b2cedadbf0
this-is-kb <kstvbora@gmail.com>
2024-05-16 15:32:47 +0530

    Complete the basic program. There are a lot of option left for improvement.

--- main.py
+++ main.py
@@ -5,8 +5,36 @@ menu = {
     'biscuit' : 10
 }
 
+bill = {
+
+}
+
 def main():
-    print("What would you like to order? ")
-    order = input(print("coffee/sandwich/pastry/biscuit "))
-    if order in menu.items():
-        order_amount += 
+    query =input("Do you like to order anything? (Yes/No) ")
+    if query == "Yes":
+        order_fn()
+        query2 = input("Do you like to order anything more? (Yes/No) ")
+        while query2 == "Yes":
+            order_fn()
+            query2 = input("Do you like to order anything more? (Yes/No) ")
+        else:
+            print_all()
+    else:
+        print("Thank you for coming. Have a good day.")
+
+def order_fn():
+    print("What do you like to order? ")
+    order = input("Coffee/sandwich/pastry/biscuit ")
+    if order in menu.keys():
+        bill.update({order:menu[order]})
+    else:
+        print(f"The item {order} is not in the menu. Please order soething else.")
+
+def print_all():
+    print("Your bill is: ")
+    for key, value in bill.items():
+        print(f"{key}={value}")
+    print(f"Total amount = {sum(bill.values())}")
+
+if __name__=="__main__":
+    main()
